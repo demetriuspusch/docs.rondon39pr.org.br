0c35d84b45983c4c8fe04fe80263b8acc41b7e21
demetrius.pusch <85627786+demetriuspusch@users.noreply.github.com>
2025-08-31 13:13:37 -0300

    a

---
+++ app.js
@@ -0,0 +1,30 @@
+let list = document.getElementById("list");
+
+const docs = [
+    {
+        url: "estatuto-rondon.pdf",
+        title: "Estatuto do Rondon"
+    }
+]
+
+function copyUrl(url, index){
+    navigator.clipboard.writeText(url);
+    let btn = document.getElementById(`copyBtn${index}`);
+    btn.innerHTML = '<i class="ph ph-check"></i>';
+    setInterval(() => {
+        btn.innerHTML = '<i class="ph ph-copy"></i>';
+    }, 2000);
+}
+
+docs.map((doc, index)=>{
+    let item = document.createElement(`div#${index}`);
+    item.className = "item";
+    
+    item.innerHTML=`
+    <i class="ph ph-file"></i>
+    <a href="/${doc.url}" rel="noopener noreferrer" target="_blank">${doc.title}</a>
+    <button onclick="copyUrl('${doc.url}, ${index}')" id="copyBtn${index}"><i class="ph ph-copy"></i></button>
+    `
+
+    list.appendChild(item);
+})
